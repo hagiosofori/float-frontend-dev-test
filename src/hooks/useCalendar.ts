dd775c8585801d4d5fec0f7656261968b3b90120
Seyram Alvin Ofori <hagiosofori@gmail.com>
2022-02-12 19:02:09 +0000

    clicking outside the date picker should close the date picker

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DatepickerProps } from '../datepicker/@types';
 import presetDates from '../datepicker/constants';
 
 export default function useCalendar(): DatepickerProps {
 	const [date, setDate] = useState({ label: presetDates[0].label, value: presetDates[0].value });
 	const [show, setShow] = useState(false);
+
+	useEffect(() => {
+		if (!show) return;
+		const eventHandler = () => {
+			setShow(false);
+		};
+		document.addEventListener('click', eventHandler);
+
+		return () => document.removeEventListener('click', eventHandler);
+	}, [show]);
 
 	return { date, setDate, show, setShow };
 }
