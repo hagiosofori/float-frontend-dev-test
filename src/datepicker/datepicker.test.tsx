75e13b04868a505936b9b253aea939a748b6f71f
Seyram Alvin Ofori <hagiosofori@gmail.com>
2022-02-12 15:08:48 +0000

    wip: for each preset date, it should be successfully selected when the user selects a relevant date

--- src/datepicker/datepicker.test.tsx
+++ src/datepicker/datepicker.test.tsx
@@ -6,6 +6,7 @@ import presetDates from './constants';
 import App from '../App';
 import { determineSelectedDateLabel } from './index';
 import { addDays } from 'date-fns';
+import format from 'date-fns/format';
 
 describe('App - Datepicker', () => {
 	function setupApp() {
@@ -42,14 +43,22 @@ describe('App - Datepicker', () => {
 		});
 
 		it(`should select the ${each.label} preset date when the correct date is selected`, () => {
-			setupApp();
-			screen.getByText(each.value.getDate()).click();
+			const { container } = setupApp();
+            const ariaLabel = format(each.value, 'd MMMM yyyy')
+			const dateTile: HTMLElement | null = container.querySelector(
+				`[aria-label="${ariaLabel}"]`
+			);
+            const _dateTile = screen.getByText(each.value.getDay())
+            console.log('value of date tile -> ', {dateTile, ariaLabel, _dateTile});
+			dateTile?.click();
+			expect(screen.getByRole('textbox')).toHaveDisplayValue(each.label);
 		});
 
 		it(`should return the correct label when given the correct date`, () => {
 			expect(determineSelectedDateLabel(each.value)).toBe(each.label);
 		});
 	});
+
 	it('should return "Custom" when no preset label is found', () => {
 		expect(determineSelectedDateLabel(addDays(new Date(), 1))).toBe('Custom');
 	});
