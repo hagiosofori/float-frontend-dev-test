d67c96fb5a8f97d5fff9c0c66e74098b12b9a662
Seyram Alvin Ofori <hagiosofori@gmail.com>
2022-02-12 16:54:23 +0000

    side bar styling

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,31 +1,50 @@
+import styled from 'styled-components';
+import colors from './colors';
+
 export default function Sidebar() {
 	return (
-		<div
-			style={{
-				maxWidth: '260px',
-				width: '20vw',
-				padding: '37px 25px',
-				background: '#F9F9F8',
-				minHeight: '100vh',
-			}}
-		>
-			{' '}
-			{/**the sidebar component */}
-			{/* //todo: is there a better way to present this side bar, than a div? */}
-			{/* //todo: hide on mobile. replace with a hamburger menu that opens and closes the sidebar */}
-			<div style={{ display: 'flex' }}>
-				<div
-					style={{
-						background: '#E3E3E0',
-						borderRadius: '50%',
-						width: '40px',
-						height: '40px',
-					}}
-				>
-					circle placeholder
+		<SidebarContainer>
+			<UserDetailsContainer>
+				<ImagePlaceholder />
+				<div>
+					<UserDetailsPlaceholder width={100} />
+					<UserDetailsPlaceholder width={60} />
 				</div>
-				<div style={{ background: '#E3E3E0' }}>user name and desc placeholder</div>
-			</div>
-		</div>
+			</UserDetailsContainer>
+		</SidebarContainer>
 	);
 }
+
+const SidebarContainer = styled.aside`
+	max-width: 260px;
+	width: 20vw;
+	padding: 37px 25px;
+	background: ${colors.lightgrey};
+	min-height: 100vh;
+
+	@media only screen and (max-width: 600px) {
+		display: none;
+	}
+`;
+
+const UserDetailsContainer = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const ImagePlaceholder = styled.div`
+	background: ${colors.darkgrey};
+	border-radius: 50%;
+	width: 40px;
+	min-width: 40px;
+	min-height: 40px;
+	height: 40px;
+	margin-right: 10px;
+`;
+
+const UserDetailsPlaceholder = styled.div<{ width: number }>`
+	background: ${colors.darkgrey};
+	height: 10px;
+	width: ${(props) => props.width}px;
+	margin-bottom: 10px;
+`;
---
+++ src/colors.ts
@@ -0,0 +1,7 @@
+const colors = {
+	teal: '',
+	darkgrey: '#E3E3E0',
+    lightgrey: '#F9F9F8'
+};
+
+export default colors;
